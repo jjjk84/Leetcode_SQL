2e075c1c8459577195e0192b625c6785061cc9f3
jjjk84 <119929581+jjjk84@users.noreply.github.com>
2023-09-30 12:11:35 +0900

    Time: 680 ms (98.04%), Space: 0B (100.00%) - LeetHub

--- 0184-department-highest-salary/0184-department-highest-salary.sql
+++ 0184-department-highest-salary/0184-department-highest-salary.sql
@@ -1,10 +1,13 @@
 /* Write your T-SQL query statement below */
-SELECT d.name AS Department
-     , e.name AS Employee
-     , e.salary AS Salary
-FROM employee AS e
-    JOIN department AS d ON e.departmentid = d.id
-    LEFT JOIN (SELECT DISTINCT departmentid AS departmentid
-                        , MAX (salary) OVER (PARTITION BY departmentid) AS max_salary
-          FROM employee) AS m ON e.departmentid = m.departmentid 
-WHERE e.salary = m.max_salary;
+SELECT sub.Department
+     , sub.Employee
+     , sub.Salary
+FROM (
+    SELECT d.name AS Department
+         , e.name AS Employee
+         , e.salary AS Salary
+         , MAX (e.salary) OVER (PARTITION BY e.departmentid) AS Max_Salary
+    FROM employee AS e
+        JOIN department AS d ON e.departmentid = d.id
+    ) AS sub
+WHERE sub.Salary = sub.Max_Salary;
